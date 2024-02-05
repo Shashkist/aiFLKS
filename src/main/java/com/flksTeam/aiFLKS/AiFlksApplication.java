eda0d79711a5ae1552292a459a183b1c4003e05d
shashkist <shashkist@gmail.com>
2024-02-05 13:55:14 +0200

    Initial commit

---
+++ src/main/java/com/flksTeam/aiFLKS/AiFlksApplication.java
@@ -0,0 +1,13 @@
+package com.flksTeam.aiFLKS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFlksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFlksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flksTeam/aiFLKS/AiFlksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flksTeam.aiFLKS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFlksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
